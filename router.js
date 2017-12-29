e4a87425bb4827699c56d2a1d314517820c4bed0
Weijie Tang <v-weijta@microsoft.com>
2017-12-29 15:50:51 +0800

    modified router

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ var img = require('./src/image.js')
 app.get('/image',img.imageRoute);
 
 var server = app.listen(3000,function(){
-	var host = server.address().address;
+	var host = '127.0.0.1';
 	var port = server.address().port;
 	console.log('Example app listen at http://%s:%s',host,port);
 });
