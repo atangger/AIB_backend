703b5be1f39ea48fb07eea4a4c2be67fd61c0ead
Weijie Tang <v-weijta@microsoft.com>
2017-12-29 13:18:39 +0800

    add the router

---
+++ router.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+	res.send('Fucking good!');
+});
+
+var server = app.listen(3000,function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listen at http://%s:%s',host,port);
+});
