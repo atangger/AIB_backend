abd5fcba18b698c8ea6cf65db186a9daf4b0250b
Michael Kong <klk1129635667@outoutlook.com>
2018-01-05 22:24:53 +0800

    Add configuration.

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
-var img = require('./src/image.js')
+var img = require('./src/image.js');
+var face = require('./src/face.js');
+var blob = require('./src/blob.js');
 
 app.get('/image',img.imageRoute);
-
+app.get('/face',face.faceRoute)
 var server = app.listen(3000,function(){
 	var host = '127.0.0.1';
 	var port = server.address().port;
 	console.log('Example app listen at http://%s:%s',host,port);
+	
 });
--- src/face.js
+++ src/face.js
@@ -1,1 +1,34 @@
 var request = require('request')
+var config = require('./config/config.js');
+exports.faceRoute = function(req,res){
+	var iH = new faceHandler('here should be the key','here should be the url');
+	iH.get();
+}
+
+function faceHandler(key,imgUrl){
+	this.key = key; // the authentication key
+	this.imgUrl = imgUrl; // the url of the src img
+}
+
+faceHandler.prototype.apiUrl = '';
+
+faceHandler.prototype.get = function() {
+	var header = {
+		 "Content-type": "application/json",
+		 "Ocp-Apim-Subscription-Key" : this.key
+	};
+
+	var options = {
+        url: this.apiUrl,
+        method: "POST",
+        headers: header,
+      	body: JSON.stringify('{"url": ' + '"' + this.imgUrl + '"}')
+      	};
+
+    request(options,function(error,response,body){
+    	if(!error && response.statusCode ==200){
+    		res.json(JSON.parse(body));
+            // 
+    	}
+    })
+};
