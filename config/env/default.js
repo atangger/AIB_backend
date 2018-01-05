9dab25ac1a306fe3ea1926f5a6f25bd8fb51806a
Merkk <1129635667@qq.com>
2018-01-05 23:01:10 +0800

    Add configuration. (#1)

---
+++ config/env/default.js
@@ -0,0 +1,20 @@
+module.exports  = {
+    face :
+    {
+      endPoint : "https://westcentralus.api.cognitive.microsoft.com/face/v1.0",
+      key1 : "d6e8c372f30544b185b010705ca3dc34",
+      key2 : "b697a37c813142beb7d18b6508335cfc"
+    },
+    text :
+    {
+      endPoint : "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0",
+      key1 : "f09009f49da4499598a8ee879650f4e3",
+      key2 : "589f8b56d3cb49118a4542a376528000"
+    },
+    cv:
+    {
+      endPoint : "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0",
+      key1 : "c09a9ff0e8dd4a2cbc3af12560fbb0cf",
+      key2 : "0e47c0392ff842e78c4df4261b773658"
+    }
+}
